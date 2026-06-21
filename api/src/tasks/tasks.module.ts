97e2c2205f31448c319ae6da7931c79ad6bb7b46
Dante Devol <dante.devol@gmail.com>
2026-06-20 21:23:12 -0400

    Wire notification emit into task/submission/message flows per recipient matrix

--- api/src/tasks/tasks.module.ts
+++ api/src/tasks/tasks.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { QuestionnairesModule } from '../questionnaires/questionnaires.module';
 import { MessagesModule } from '../messages/messages.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TaskStatusService } from './task-status.service';
 
 @Module({
-  imports: [QuestionnairesModule, MessagesModule],
+  imports: [QuestionnairesModule, MessagesModule, NotificationsModule],
   controllers: [TasksController],
   providers: [TasksService, TaskStatusService],
   exports: [TasksService, TaskStatusService],
